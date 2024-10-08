352ca16b3e5b8235f2a00c7f0ef4c4b549b28668
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-10-08 10:24:47 +0300

    feature: added comments in parsing3.c:88-108

--- src/parsing3.c
+++ src/parsing3.c
@@ -6,7 +6,7 @@
 /*   By: fdessoy- <fdessoy-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:12:41 by fdessoy-          #+#    #+#             */
-/*   Updated: 2024/10/08 10:18:26 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2024/10/08 10:24:18 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,28 @@ static int8_t	ea_sprite(t_data *data)
 	}	
 }
 
+/**
+ * search_sprites() is mainly separated from the rest of the parsing
+ * just for memory management. Given the repetitive nature, it is possible
+ * to observe plainly that it does the same task with few differences:
+ * 
+ * 1. `no_sprite()` takes the north wall texture.
+ * 2. `so_sprite()` takes the south wall texture.
+ * 3. `we_sprite()` takes the west wall texture.
+ * 4. `ea_sprite()` takes the east wall texture.
+ * 
+ * All these functions are saving the whole strings that are found in the
+ * `.cub` file and parse out the empty spaces leaving only the path to the
+ * sprite given by the file. The repetitive nature was a design choice to be
+ * able to manage the memory in case malloc() fails at any point, since we 
+ * extract the information using ft_strdup().
+ * 
+ * Upon successful extraction of all string, we simply return SUCCESS to the
+ * caller function. All the information has been saved in the `t_data` struct.
+ * 
+ * This part of the code **DOES NOT** take care of checking if the path if 
+ * valid or not. That will be taken care by the MLX functions later on.
+ */
 int8_t	search_sprites(t_data *data)
 {
 	if (no_sprite(data) == FAILURE)
