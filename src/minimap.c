997d62358a5704b4a22be110ec9592bfc7f4583e
Alexandra Raveala <araveala@c2r1p6.hive.fi>
2024-10-17 08:26:36 +0300

    fix: segv on drawing of new minimap when out of map size fixed

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: araveala <araveala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 08:32:58 by araveala          #+#    #+#             */
-/*   Updated: 2024/10/16 18:07:29 by araveala         ###   ########.fr       */
+/*   Updated: 2024/10/17 08:23:53 by araveala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -284,9 +284,38 @@ int	init_mini_imgs(t_data *data)
 		return (err_msg(NULL, IMAGE_FAIL, FAILURE));
 	mlx_resize_image(data->im_mini_floor, MINI_T, MINI_T);//T_SIZE, T_SIZE);
 	mlx_resize_image(data->im_mini_wall, MINI_T, MINI_T);//T_SIZE, T_SIZE);
-	return (0);
+	return (0);/*void init_map(t_data *data) {
+    int x = 0;
+    int y = 0;
+    int x_start = (int)data->x_ppos - (MINI_WIDTH / 2);
+    int y_start = (int)data->y_ppos - (MINI_HEIGHT / 2);
+    int x_end = x_start + MINI_WIDTH;
+    int y_end = y_start + MINI_HEIGHT;
+
+    data->im_map = mlx_new_image(data->mlx, MINI_WIDTH, MINI_HEIGHT);
+    while (y < y_end)
+	{
+		y = y_start;
+        while ( x < x_end)
+		{
+			x = x_start;
+            if (x >= 0 && x < data->map_width && y >= 0 && y < data->map_length)
+			{
+                if (data->map[y][x] == '0' || data->map[y][x] == 'N' || data->map[y][x] == '1')
+                {
+				    mlx_image_to_window(data->mlx, data->im_mini_floor, (x - x_start) * MINI_T, (y - y_start) * MINI_T);
+                    mlx_image_to_window(data->mlx, data->im_mini_wall, (x - x_start) * MINI_T, (y - y_start) * MINI_T);
+                }
+            }
+			x++;
+        }
+		y++;
+    }
+    mlx_image_to_window(data->mlx, data->im_map, 200, 200);
+}*/
 }
-void	calc(int *p_x, int *p_y)
+
+void	adjust_mapstart(int *p_x, int *p_y)
 {
 	if (*p_x >= 3)
 		(*p_x) -= 3;
@@ -329,35 +358,6 @@ void	calc(int *p_x, int *p_y)
 		y++;
 	}
 }*/
-/*void init_map(t_data *data) {
-    int x = 0;
-    int y = 0;
-    int x_start = (int)data->x_ppos - (MINI_WIDTH / 2);
-    int y_start = (int)data->y_ppos - (MINI_HEIGHT / 2);
-    int x_end = x_start + MINI_WIDTH;
-    int y_end = y_start + MINI_HEIGHT;
-
-    data->im_map = mlx_new_image(data->mlx, MINI_WIDTH, MINI_HEIGHT);
-    while (y < y_end)
-	{
-		y = y_start;
-        while ( x < x_end)
-		{
-			x = x_start;
-            if (x >= 0 && x < data->map_width && y >= 0 && y < data->map_length)
-			{
-                if (data->map[y][x] == '0' || data->map[y][x] == 'N' || data->map[y][x] == '1')
-                {
-				    mlx_image_to_window(data->mlx, data->im_mini_floor, (x - x_start) * MINI_T, (y - y_start) * MINI_T);
-                    mlx_image_to_window(data->mlx, data->im_mini_wall, (x - x_start) * MINI_T, (y - y_start) * MINI_T);
-                }
-            }
-			x++;
-        }
-		y++;
-    }
-    mlx_image_to_window(data->mlx, data->im_map, 200, 200);
-}*/
 
 void	init_map(t_data *data)
 {
@@ -366,23 +366,13 @@ void	init_map(t_data *data)
 
 	x = 0;
 	y = 0;
-	int count_boxes = 0;
 	data->im_map = mlx_new_image(data->mlx, MINI_WIDTH, MINI_HEIGHT);
-	while (y < 7)//data->map_length)
+	while (y < 7)
 	{
-		printf("what char = %c\n", data->map[y][x]);
-		while (x < 7)//data->map_width)
+		while (x < 7)
 		{
-			//if (data->map[y][x] == '0' || data->map[y][x] == 'N' || data->map[y][x] == '1') // n char 
-			//{ 
 			mlx_image_to_window(data->mlx, data->im_mini_floor, x * MINI_T, y * MINI_T);
 			mlx_image_to_window(data->mlx, data->im_mini_wall, x * MINI_T, y * MINI_T);
-			count_boxes++;
-			printf("tell me boxes = %d\n", count_boxes);
-			//data->im_mini_floor->instances[0].enabled = 0;
-			//data->im_mini_wall->instances[0].enabled = 0;
-
-			//}
 			x++;
 		}
 		x = 0;
@@ -391,16 +381,13 @@ void	init_map(t_data *data)
 	mlx_image_to_window(data->mlx, data->im_map, 200, 200); // 200 200 
 	y = 0;
 	x = 0;
-	int index = 0;//y * data->map_width + x;
-	while (y < 7)//data->map_length)
+	int index = 0;
+	while (y < 7)
 	{
 		while (x < 7)
 		{
 			if (data->map[y][x] == '0' || data->map[y][x] == 'N' || data->map[y][x] == '1') // n char 
 			{ 
-				//mlx_image_to_window(data->mlx, data->im_mini_floor, x * MINI_T, y * MINI_T);
-				//mlx_image_to_window(data->mlx, data->im_mini_wall, x * MINI_T, y * MINI_T);
-				//printf("what is index wwwwwwwwwwwwwwwwwwwww= %d\n", index);
 				data->im_mini_floor->instances[index].enabled = 0;
 				data->im_mini_wall->instances[index].enabled = 0;
 			}
@@ -418,147 +405,33 @@ void	draw_map(t_data *data)
 	int y = 0;
 	int p_x = (int)data->x_ppos;
 	int p_y = (int)data->y_ppos;
-	calc(&p_x, &p_y);
-	int index = 0;///y * data->map_width + x;
-	printf("starting at x = %d and y = %d\n", p_x, p_y);
-	//int new start = ;
-	while (y < 7 && index < 36)
+	int index = 0;
+	
+	adjust_mapstart(&p_x, &p_y);
+	while (y < 7)// && ++y)
 	{
-		while (x < 7 && index < 36)
+		while (x < 7)// && ++x)
 		{
-	        if ((p_y + y) > data->map_length || (p_x + x) > data->map_width)
-			{
-				printf("breaaaaaaak\n");
+	        if ((p_y + y) >= data->map_length || (p_x + x) >= data->map_width)
 				break ;
-			}
 			if (data->map[p_y + y][p_x + x] == '1')
 			{
-	            printf("whats index here walll= %d\n", index);
 				data->im_mini_wall->instances[index].enabled = 1;
 				data->im_mini_floor->instances[index].enabled = 0;
 			}
 			else if (data->map[p_y + y][p_x + x] == '0')
 			{
-	            printf("whats index here floor= %d\n", index);
 				data->im_mini_wall->instances[index].enabled = 0;
 				data->im_mini_floor->instances[index].enabled = 1;
-	            //index++;
 			}
 			index++;
 			x++;
-			
-			//p_x++;
         }
 		x = 0;
 		y++;
-		
 	}
-/*	while (y < 7)
-	{
-        if ((p_y + y) >= data->map_length || (p_x + x) >= data->map_width)
-			break ;
-        while (x < 7) // 6 is how many these size tiles fit 
-		{	 
-			if ((p_y + y) > data->map_length || (p_x + x) > data->map_width)
-			{
-				printf("breaaaaaaak\n");
-				break ;
-			}
-			else if (data->map[p_y + y][p_x + x] == '1')
-			{
-	            printf("whats index here = %d\n", index);
-				data->im_mini_wall->instances[index].enabled = 1;
-				data->im_mini_floor->instances[index].enabled = 0;
-				index++;
-			}
-			else if (data->map[p_y + y][p_x + x] == '0')
-			{
-	            printf("whats index here = %d\n", index);
-				data->im_mini_wall->instances[index].enabled = 0;
-				data->im_mini_floor->instances[index].enabled = 1;
-				index++;
-            }
-			x++;
-        }
-		x = 0;
-		y++;
-
-			//if (p_y < 0 || p_x < 0)
-			//	break ;
-			//int xx = p_x + x;
-			//int yy = p_y + y;
-			printf("nums py + y = %d px + x  = %d\n", p_y + y, p_x + x);
-    }*/
-	printf("%d\n", 6 * 6);
-	printf("exiting\n");
 }
 	
-/*void	draw_map(t_data *data)
-{
-	int x = 0;
-	int y = 0;
-	int p_x = (int)data->x_ppos;
-	int p_y = (int)data->y_ppos;
-	double center_x;
-	double center_y;
-	int draw_x = 0;
-	int draw_y = 0;
-	//int radius = 10/ 2;
-	
-	data->im_map = mlx_new_image(data->mlx, MINI_WIDTH, MINI_HEIGHT);
-	center_x = (int)(data->x_ppos - p_x) * MINI_T / 2;
-	center_y = (int)(data->y_ppos - p_y) * MINI_T / 2;
-	calc(&p_x, &p_y);
-	while (y < 7)
-	{
-        if ((p_y + y) >= data->map_length || (p_x + x) >= data->map_width)
-			break ;
-        //if (p_y < 0 || p_x < 0)
-		//	break ;
-        while (x < 7) // 6 is how many these size tiles fit 
-		{
-			draw_x = x * MINI_T;
-			draw_y = y * MINI_T;
-	        if ((p_y + y) >= data->map_length || (p_x + x) >= data->map_width)
-				break ;
-    	    //if (p_y < 0 || p_x < 0)
-			//	break ;
-			//printf("bug hunting 1\n");
-            if (data->map[p_y + y][p_x + x] == '1')
-			{
-				mlx_image_to_window(data->mlx, data->im_mini_wall, draw_x, draw_y);
-            }
-			else if (data->map[p_y + y][p_x + x] == '0')
-			{
-				mlx_image_to_window(data->mlx, data->im_mini_floor, draw_x, draw_y);
-            }
-			//printf("bug hunting 2\n");
-			x++;
-        }
-		x = 0;
-		y++;
-    }*/
-	/*y = -radius;
-	while (y <= radius)
-	{
-		x = -radius;
-		while (x <= radius)
-		{
-			if ((x * x) + (y * y) <= (radius * radius))
-			{
-				draw_x = center_x + x;
-				draw_y = center_y + y;
-				if (draw_x >= 0 && draw_x < MINI_WIDTH && draw_y >= 0 && draw_y < MINI_HEIGHT)
-					mlx_put_pixel(data->im_map, draw_x, draw_y, 0xFF0000FF);
-			}
-			x++;
-		}
-		y++;
-	}*/
-//mlx_put_pixel(data->im_map, center_x, center_y, 0xFF0000FF);  // Player color
-//center_x = (int)(data->x_ppos) + MINI_T / 2;
-//center_y = (int)(data->y_ppos) + MINI_T / 2;
-
 //adjust_tilesize(data); maybe
 int	initlize_minimap(t_data *data)
 {
@@ -568,6 +441,7 @@ int	initlize_minimap(t_data *data)
 	
 	//init_mini_floor(data);
 	//init_mini_walls(data);
+	//drawing onto big sxreen
 	init_mini_player(data);
 	return (0);
 }
