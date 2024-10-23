24165a5f65c319ff7c5e30879c0d2676bd1d555c
sparklebutt <arandomsanti@gmail.com>
2024-10-23 16:29:36 +0300

    fix: maybe a little faster, there is too many calculations, must simplify

--- src/init_higher_dimension.c
+++ src/init_higher_dimension.c
@@ -6,7 +6,7 @@
 /*   By: araveala <araveala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 10:50:15 by araveala          #+#    #+#             */
-/*   Updated: 2024/10/23 16:10:57 by araveala         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:26:08 by araveala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,6 +104,7 @@ static uint32_t rgb(t_data *data, int x, int y, int i)
 	uint32_t	g;
 	uint32_t	b;
 	uint32_t	a;
+	int pos;
 	
 	if (data->ray_hit[i] == NORTH)
 	{
@@ -124,10 +125,11 @@ static uint32_t rgb(t_data *data, int x, int y, int i)
 		data->im_current_wall = data->im_w_wall;
 	if (data->im_current_wall != NULL)
 	{
-		r = data->im_current_wall->pixels[(y * data->im_current_wall->width + x) * 4]; //4 bytes per pixel
-		g = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 1];
-		b = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 2];
-		a = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 3];
+		pos = (y * data->im_current_wall->width + x) * 4;
+		r = data->im_current_wall->pixels[pos]; //4 bytes per pixel
+		g = data->im_current_wall->pixels[pos + 1];
+		b = data->im_current_wall->pixels[pos + 2];
+		a = data->im_current_wall->pixels[pos + 3];
 		return (load_rgb(r, g, b, a));
 	}
 	return (0);
@@ -140,11 +142,11 @@ int	draw_wall(t_data *data, int i, int x, double img_y)
 	double current_wall_pos;
 	double img_x; 
 	double img_y_inc;
-	uint32_t colour;
+	uint32_t colour = 0;
 	double distance_to_plane = (WIDTH / 2) / tan(FOV / 2 * PI / 180);
 	double wall_scale_factor = 0.01; // or STEP? 
 
-	wall_h = HEIGHT / data->ray_len[i] * distance_to_plane * wall_scale_factor; // 8 needs to be a fluctuating number . // we get the height of the wall based on len
+	wall_h = HEIGHT / data->ray_len[i] * distance_to_plane * wall_scale_factor; // we get the height of the wall based on len
 	top_of_wall = (HEIGHT - wall_h) / 2; // we set our starting point to the top of where thw all begins
 	current_wall_pos = top_of_wall; // we set our incrementer 
 	img_x = (double)i / RAY_MAX * data->im_n_wall->width;
@@ -156,13 +158,10 @@ int	draw_wall(t_data *data, int i, int x, double img_y)
 		if (x >= 0 && x < WIDTH && current_wall_pos >= 0 && current_wall_pos < HEIGHT)
 		{
 			img_y = (current_wall_pos - top_of_wall) / wall_h * data->im_n_wall->height;
-			if (img_x < 0 || img_x >= data->im_n_wall->width || img_y < 0 || img_y >= data->im_n_wall->height)
-			{
- 				   printf("Out of bounds - img_x: %f, img_y: %f\n", img_x, img_y);
-			}
 			if (x >= 0 && x < WIDTH && data->ray_len[i] > 0)
 			{
-				colour = rgb(data, img_x, img_y, i);
+				if (img_y >= 0 && img_y < data->im_n_wall->height)
+					colour = rgb(data, img_x, img_y, i);
 				// this is a segv protection
 				if (colour == 0)
 					break ;
@@ -171,6 +170,6 @@ int	draw_wall(t_data *data, int i, int x, double img_y)
 		}
 		current_wall_pos++;
 		img_y += img_y_inc;
-	}	
+	}
 	return (SUCCESS);
 }
