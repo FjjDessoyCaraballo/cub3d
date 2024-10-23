836fdf87d1ba74fcf74246c85ec35755990ef76d
sparklebutt <arandomsanti@gmail.com>
2024-10-23 16:13:17 +0300

    fix: segfault : png pixel[position] was going out of boundries , we are still very slow

--- src/init_higher_dimension.c
+++ src/init_higher_dimension.c
@@ -6,7 +6,7 @@
 /*   By: araveala <araveala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 10:50:15 by araveala          #+#    #+#             */
-/*   Updated: 2024/10/23 14:53:39 by araveala         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:10:57 by araveala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,6 +104,7 @@ static uint32_t rgb(t_data *data, int x, int y, int i)
 	uint32_t	g;
 	uint32_t	b;
 	uint32_t	a;
+	
 	if (data->ray_hit[i] == NORTH)
 	{
 		//printf("north\n");
@@ -121,57 +122,16 @@ static uint32_t rgb(t_data *data, int x, int y, int i)
 	}
 	else if (data->ray_hit[i] == WEST)
 		data->im_current_wall = data->im_w_wall;
-
-	//int position = (y * width + x) * bytes_per_pixel;
-	r = data->im_current_wall->pixels[(y * data->im_current_wall->width + x) * 4]; //4 bytes per pixel
-	g = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 1];
-	b = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 2];
-	a = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 3];
-	return (load_rgb(r, g, b, a));
-}
-
-/*int	draw_section(t_data *data, double wall_h, double wall_i, int x)
-{
-	double wall_bottom;
-	double img_y;
-	double img_y_step;
-	double img_x;
-
-	wall_bottom = wall_i + wall_h;
-	img_y = 0;
-	img_y_step = (double)data->im_n_wall->height / wall_h;
-	img_x = (double)x / RAY_MAX * data->im_n_wall->width;
-	//x = x * (WIDTH / RAY_MAX);
-	while (wall_i < wall_bottom)
+	if (data->im_current_wall != NULL)
 	{
-		if (wall_i > 0 && wall_i < HEIGHT)
-		{
-			if (x >= 0 && x < WIDTH && data->ray_len[x] > 0)
-				mlx_put_pixel(data->im_ray, x * (WIDTH / RAY_MAX), (int)wall_i, rgb(data, img_x, img_y));
-		}
-//		current_wall_pos += increment;
-		wall_i++;
-		img_y += img_y_step;
-	}	
-	return (SUCCESS);
+		r = data->im_current_wall->pixels[(y * data->im_current_wall->width + x) * 4]; //4 bytes per pixel
+		g = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 1];
+		b = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 2];
+		a = data->im_current_wall->pixels[((y * data->im_current_wall->width + x) * 4) + 3];
+		return (load_rgb(r, g, b, a));
+	}
+	return (0);
 }
-
-
-int draw_wall(t_data *data, int i, int img_x, int x)
-{
-	double	wall_h;
-	double wall_top;
-	//double current_wall_pos;
-	//double	img_y_step;
-	(void)img_x;
-	(void)x;
-	//x = i * (WIDTH / RAY_MAX);
-	wall_h = HEIGHT / data->ray_len[i];
-	wall_top = (HEIGHT - wall_h) / 2;
-	clear_img(data, i);
-	draw_section(data, wall_h, wall_top, i);
-	return (SUCCESS);
-}*/
 
 int	draw_wall(t_data *data, int i, int x, double img_y)
 {
@@ -184,7 +144,6 @@ int	draw_wall(t_data *data, int i, int x, double img_y)
 	double distance_to_plane = (WIDTH / 2) / tan(FOV / 2 * PI / 180);
 	double wall_scale_factor = 0.01; // or STEP? 
 
-	//printf("check = %f\n", distance_to_plane);
 	wall_h = HEIGHT / data->ray_len[i] * distance_to_plane * wall_scale_factor; // 8 needs to be a fluctuating number . // we get the height of the wall based on len
 	top_of_wall = (HEIGHT - wall_h) / 2; // we set our starting point to the top of where thw all begins
 	current_wall_pos = top_of_wall; // we set our incrementer 
@@ -194,11 +153,19 @@ int	draw_wall(t_data *data, int i, int x, double img_y)
 	clear_img(data, i);
 	while (current_wall_pos < top_of_wall + wall_h)
 	{
-		if (x >= 0 && x < WIDTH && current_wall_pos > 0 && current_wall_pos < HEIGHT)
+		if (x >= 0 && x < WIDTH && current_wall_pos >= 0 && current_wall_pos < HEIGHT)
 		{
+			img_y = (current_wall_pos - top_of_wall) / wall_h * data->im_n_wall->height;
+			if (img_x < 0 || img_x >= data->im_n_wall->width || img_y < 0 || img_y >= data->im_n_wall->height)
+			{
+ 				   printf("Out of bounds - img_x: %f, img_y: %f\n", img_x, img_y);
+			}
 			if (x >= 0 && x < WIDTH && data->ray_len[i] > 0)
 			{
 				colour = rgb(data, img_x, img_y, i);
+				// this is a segv protection
+				if (colour == 0)
+					break ;
 				mlx_put_pixel(data->im_ray, x, (int)current_wall_pos, colour);
 			}
 		}
